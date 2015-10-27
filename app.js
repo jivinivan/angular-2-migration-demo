54bdf0d65bf0b0e1a20c3770707021adacd2a29a
Corey Fields <corey@getambassador.com>
2015-10-27 10:58:15 -0400

    moved business logic into service

--- app.js
+++ app.js
@@ -3,26 +3,35 @@
 angular
   .module('pongScoreApp', [])
   .controller('pongScoreCtrl', pongScoreCtrl)
-  .directive('pongScoreForm', pongScoreDirective);
+  .directive('pongScoreForm', pongScoreDirective)
+  .service('pongScoreService', pongScoreService);
+
+function pongScoreService() {
+  this.player = {
+    wins: 0,
+    losses: 0
+  };
+}
+pongScoreService.prototype.addWin = function() {
+  this.player.wins++;
+}
+pongScoreService.prototype.addLoss = function() {
+  this.player.losses++;
+}
 
 function pongScoreCtrl() {}
 
 function pongScoreDirective() {
   return {
     controllerAs: 'formCtrl',
-    controller: [function(){
-      this.player = {
-        wins: 0,
-        losses: 0
-      };
-
-      this.addWin = function() {
-        this.player.wins++;
-      };
-
-      this.addLoss = function() {
-        this.player.losses++;
-      };
+    controller: ['pongScoreService', function(pongScoreService) {
+      this.player = pongScoreService.player;
+      this.addWin = function addWin() {
+        pongScoreService.addWin();
+      }
+      this.addLoss = function addLoss() {
+        pongScoreService.addLoss();
+      }
     }]
   };
 }
