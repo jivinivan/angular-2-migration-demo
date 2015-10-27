dab52b630af363aaa2dc4808f4a5768d751a150c
Ivan Sugerman <ivan@getambassador.com>
2015-10-26 21:27:28 -0400

    created simple es5 / angular 2 version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # angular-2-migration-demo
+
+### To run local server
+
+$ npm install -g node-static
+$ static
+
+Once static server has started you should be able to access the site at [http://127.0.0.1:8080/](http://127.0.0.1:8080/)
--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 'use strict';
 
-angular
-  .module('pongScoreApp', [])
-  .controller('pongScoreCtrl', pongScoreCtrl)
-  .directive('pongScoreForm', pongScoreDirective);
-
-function pongScoreCtrl() {}
-
-function pongScoreDirective() {
-  return {
-    controllerAs: 'formCtrl',
-    controller: [function(){
-      this.player = {
-        wins: 0,
-        losses: 0
-      };
-
-      this.addWin = function() {
-        this.player.wins++;
-      };
-
-      this.addLoss = function() {
-        this.player.losses++;
-      };
-    }]
+function playerService() {
+  this.player = {
+    wins: 0,
+    losses: 0
   };
 }
+
+playerService.prototype.addWin = function(){
+  this.player.wins++;
+};
+
+playerService.prototype.addLoss = function(){
+  this.player.losses++;
+};
+
+var pongScoreForm = ng.
+  Component({
+    selector: 'pong-score-form',
+    properties: ['person'],
+    bindings: [playerService]
+  }).
+  View({
+    templateUrl: './pong-score-form.html'
+  }).
+  Class({
+    constructor: [playerService, function(playerService){
+      this.playerService = playerService;
+    }]
+  });
+
+var pongScoreApp = ng.
+  Component({
+    selector: 'pong-score-app',
+    bindings: [playerService]
+  }).
+  View({
+    templateUrl: './pong-score-app.html',
+    directives: [pongScoreForm]
+  }).
+  Class({
+    constructor: [function(){}]
+  });
+
+ng.bootstrap(pongScoreApp);
